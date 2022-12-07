e891bd996223f8d0d1731fc16827d6af1012dbdf
User <user@its.me>
2022-12-07 07:37:02 +0100

    Generic repository using CrudRepository interface

--- src/main/java/online/javafun/bikerent/BikeRepository.java
+++ src/main/java/online/javafun/bikerent/BikeRepository.java
@@ -1,30 +1,5 @@
 package online.javafun.bikerent;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-@Repository
-public class BikeRepository {
-    private final EntityManager entityManager;
-
-    public BikeRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-    @Transactional
-    public void save(Bike bike) {
-        entityManager.persist(bike);
-    }
-
-    public Optional<Bike> findById(Long id) {
-        Bike bike = entityManager.find(Bike.class, id);
-        return Optional.ofNullable(bike);
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        findById(id).ifPresent(entityManager::remove);
-    }
-}
+interface BikeRepository extends CrudRepository<Bike, Long> {}
